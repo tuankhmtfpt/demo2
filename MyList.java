a29a1c299d27eba03b84a81bb1e90530579eb4db
tuankhmtfpt <71730826+tuankhmtfpt@users.noreply.github.com>
2021-10-28 11:40:14 +0700

    Add files via upload

---
+++ MyList.java
@@ -0,0 +1,124 @@
+
+public class MyList {
+
+    Node head, tail;
+    
+    // (1)
+    MyList() {
+        
+    }
+    // (2)
+    boolean isEmpty() {
+        
+        return false;
+    }
+    // (3)
+    void clear() {
+        
+    }
+
+    // (4) 
+    void addLast(Person x) {
+        
+    }
+    // (5)
+    void visit(Node p) {
+        
+    }
+    // (6)
+    void traverse() {
+        
+    }
+    // (7) optional
+    void addMany(String[] a, int[] b) {
+        
+    }
+
+    // (8)
+    Node searchByName(String xName) {
+        
+        return null;
+    }
+
+    // (9)
+    void addFirst(Person x) {
+    }
+
+    // (10)
+    void insertAfter(Node q, Person x) {
+    }
+
+    // (11)
+    void insertBefore(Node q, Person x) {
+    }
+
+    // (12)
+    void remove(Node q) {
+    }
+
+    // (13)
+    void remove(String xName) {
+    }
+
+    // (14)
+    void remove(int xAge) {
+    }
+
+    // (15)
+    void removeAll(int xAge) {
+    }
+
+    // (16)
+    Node pos(int k) {
+        return (null);
+    }
+
+    // (17)
+    void removePos(int k) {
+    }
+
+    // (18)
+    void sortByName() {
+    }
+
+    // (19)
+    void sortByAge() {
+    }
+
+    // (20)
+    int size() {
+        return (0);
+    }
+
+    // (21)
+    Person[] toArray() {
+        return (null);
+    }
+
+    // (22)
+    void reverse() {
+    }
+
+    // (23) 
+    Node findMaxAge() {
+        return (null);
+    }
+
+    // (24) 
+    Node findMinAge() {
+        return (null);
+    }
+
+    // (24) 
+    void setData(Node p, Person x) {
+    }
+
+    // (26) 
+    void sortByAge(int k, int h) {
+    }
+
+    // (27) 
+    void reverse(int k, int h) // reverse from k to h 
+    {
+    }
+}
